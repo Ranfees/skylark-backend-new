18f8e0e14eb98208815ab473a02d6317c3f0a990
nadiyakm <kmnadiya003@gmail.com>
2025-12-25 09:04:52 +0530

    Auth: prevent duplicate user registration

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,36 +1,31 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-exports.isLoggedin = (req, res, next) => {
-    try {
-        const token = req.cookies.token
-        if (!token) {
-            return res.clearCookie('token').status(401).json({message:'no token in middleware'})
-          
-        }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = decoded
-        return next()
-    } catch (e) {
-        console.log(e)
-        return res.clearCookie('token').status(401).json({message:'Please login again'})
-       
-    }
-}
+exports.isLoggedin = async (req, res, next) => {
+	const token = req.cookies.token;
+	if (!token) return res.clearCookie('token').status(401).json({ message: 'No token found' });
+
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = decoded;
+		next();
+	} catch (e) {
+		return res.status(401).json({ message: 'Invalid token' });
+	}
+};
 
 exports.isAdmin = (req, res, next) => {
-    try {
-      if (!req.user || req.user.role !== 'admin') {
-        return res.status(403).json({
-          message: 'Access denied: Admins only'
-        })
-      }
-  
-      next()
-    } catch (err) {
-      console.log(err)
-      return res.status(401).json({
-        message: 'Unauthorized'
-      })
-    }
-  }
-  
+	try {
+		if (!req.user || req.user.role !== 'admin') {
+			return res.status(403).json({
+				message: 'Access denied: Admins only',
+			});
+		}
+
+		next();
+	} catch (err) {
+		console.log(err);
+		return res.status(401).json({
+			message: 'Unauthorized',
+		});
+	}
+};
--- models/User.js
+++ models/User.js
@@ -1,52 +1,83 @@
 const mongoose = require('mongoose');
-const bcrypt=require('bcryptjs')
-const jwt=require('jsonwebtoken')
-const userSchema = new mongoose.Schema({
-  id: String,
-  username: String,
-  name: String,
-  phone: String,
-  mail: String,
-  role: {
-    type: String,
-    default: "user"
-  },
-  password: String,
-  hasbooking: Boolean,
-  dp: String,
-  otp: String
-}, {
-  timestamps: true  
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+// const userSchema = new mongoose.Schema({
+//   id: String,
+//   username: String,
+//   name: String,
+//   phone: String,
+//   mail: String,
+//   role: {
+//     type: String,
+//     default: "user"
+//   },
+//   password: String,
+//   hasbooking: Boolean,
+//   dp: String,
+//   otp: String
+// }, {
+//   timestamps: true
+// });
+const userSchema = new mongoose.Schema(
+	{
+		id: String,
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		phone: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		mail: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		role: {
+			type: String,
+			default: 'user',
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		hasbooking: Boolean,
+		dp: String,
+		otp: String,
+	},
+	{ timestamps: true }
+);
+
+userSchema.pre('save', async function () {
+	if (!this.isModified('password')) {
+		return;
+	}
+
+	this.password = await bcrypt.hash(this.password, 10);
+	return;
 });
 
+userSchema.methods.isValidatedPassword = async function (userSendPassword) {
+	return await bcrypt.compare(userSendPassword, this.password);
+};
 
-userSchema.pre('save',async function(){
+userSchema.methods.getJwtToken = function () {
+	return jwt.sign(
+		{
+			id: this.id,
+			name: this.name,
+			role: this.role,
+		},
+		process.env.JWT_SECRET,
+		{ expiresIn: '8h' }
+	);
+};
 
-    if(!(this.isModified('password'))){
-        return
-    }
-
-    this.password= await bcrypt.hash(this.password,10)
-    return
-
-})
-
-
-userSchema.methods.isValidatedPassword = async function(userSendPassword){
-    return await bcrypt.compare(userSendPassword,this.password)
-}
-
-
-userSchema.methods.getJwtToken = function(){
-    return jwt.sign(
-        {    
-            id:this.id,
-            name:this.name,
-            role:this.role
-        },
-        process.env.JWT_SECRET,
-        {expiresIn:'8h'}
-    )
-} 
-
-module.exports =  mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
